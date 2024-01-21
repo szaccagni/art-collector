facb3bfe13f3300dd47b5d63efbbe887159c10ba
szaccagni <sarahzaccagni@gmail.com>
2024-01-21 18:35:41 -0500

    add about this project

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,15 @@
 import { Link, useNavigate } from 'react-router-dom'
 import * as userService from '../../utilities/users-service' 
 import {useState} from 'react'
-import { Dialog } from '@mui/material'
+import { Dialog, Box } from '@mui/material'
 import LoginForm from '../../components/LoginForm/LoginForm'
 import SignUpForm from '../../components/SignUpForm/SignUpForm'
+import CloseIcon from '@mui/icons-material/Close';
 
 export default function NavBar({ user, setUser, active, setActive, getBoards}) {
     const [loginModalOpen, setLoginModalOpen] = useState(false)
     const [signUpModalOpen, setSignUpModalOpen] = useState(false)
+    const [aboutModalOpen, setAboutModalOpen] = useState(false)
 
     const activeStyle = {
         textDecoration: 'underline',
@@ -51,10 +53,15 @@ export default function NavBar({ user, setUser, active, setActive, getBoards}) {
                 </div>
                 </>
                 :
-                <div style={{display: 'flex', justifyContent: 'right', width: '100%'}}>
+                <>
+                <div style={{display: 'flex', justifyContent: 'left', width: '20%'}}>
+                    <Link to="" onClick={() => setAboutModalOpen(true)} className='nav-link'>About This Project</Link>
+                </div>
+                <div style={{display: 'flex', justifyContent: 'right', width: '80%'}}>
                     <div><Link to="" onClick={() => setLoginModalOpen(true)} className='nav-link'>Log In</Link></div>
                     <div><Link to="" onClick={() => setSignUpModalOpen(true)} className='nav-link'>Sign Up</Link></div>
                 </div>
+                </>
             }
             </nav>
             <Dialog onClose={() => setLoginModalOpen(false)} open={loginModalOpen}>
@@ -62,6 +69,17 @@ export default function NavBar({ user, setUser, active, setActive, getBoards}) {
             </Dialog>
             <Dialog onClose={() => setSignUpModalOpen(false)} open={signUpModalOpen}>
                 <SignUpForm setUser={setUser} setSignUpModalOpen={setSignUpModalOpen}/>
+            </Dialog>
+            <Dialog onClose={() => setAboutModalOpen(false)} open={aboutModalOpen}>
+                <Box style={{textAlign: 'right', paddingRight: '15px', paddingTop: '10px'}}><CloseIcon onClick={() => setAboutModalOpen(false)} sx={{cursor: 'pointer'}} /></Box>
+                <Box sx={{padding: ' 15px 25px 25px 25px'}}>
+                    <Box>
+                        This project, Art Collector, was submitted as my capstone project for General Assembly's software engineering immersive bootcamp. It is a MERN app built to show case the art of The Rijks Museum in Amsterdam and The Metropolitan Museum of Art in New York City. 
+                    </Box>
+                    <Box sx={{marginTop: '12px'}}>
+                        This application is a work in progress. Read more about it <Link to="https://github.com/szaccagni/art-collector#readme" target='_blank'>here</Link> 
+                    </Box>
+                </Box>
             </Dialog>
         </>
     )
